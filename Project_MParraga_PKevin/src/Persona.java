b85e445049cbc98578598973499c6e3b0d799a6b
User <User@192.168.129.1>
2021-02-27 12:15:50 -0500

    Tercer commit: Agregar código a clase Persona Kevin Paute

--- Project_MParraga_PKevin/src/Persona.java
+++ Project_MParraga_PKevin/src/Persona.java
@@ -1,4 +1,47 @@
 
 public class Persona {
+	
+	//Atributos
+	protected String nombre;
+	protected int cedula;
+	
+	//Constructores
+	public Persona(String nombre, int cedula) {
+		this.nombre = nombre;
+		this.cedula = cedula;
+	}
+	
+	public Persona() {
+		this.nombre = "";
+		this.cedula = 0;
+	}
+	
+	public Persona(String nombre) {
+		this.nombre = "nombre";
+		this.cedula = 0;
+	}
 
+	//Metodos getters y setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+	
+	//Metodo to String
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", cedula=" + cedula + "]";
+	}
+	
 }
