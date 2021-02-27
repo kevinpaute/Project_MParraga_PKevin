226f687f7a577fe2331ca16fa109600a124bb7d4
User <User@192.168.129.1>
2021-02-27 12:30:49 -0500

    Cuarto commit: Agregar atributos y constructor a subclases Maria Jose Parraga

--- Project_MParraga_PKevin/src/Empleado.java
+++ Project_MParraga_PKevin/src/Empleado.java
@@ -1,4 +1,13 @@
 
-public class Empleado {
-
+public class Empleado extends Persona{ //Extension de la clase Persona
+	
+	private String cargo;
+	private String sueldo;
+	
+	//CONSTRUCTOR
+	public Empleado(String nombre, int cedula,String cargo, String sueldo) {
+		super(nombre, cedula);
+		this.cargo = cargo;
+		this.sueldo = sueldo;
+	}
 }
--- Project_MParraga_PKevin/src/Estudiante.java
+++ Project_MParraga_PKevin/src/Estudiante.java
@@ -1,4 +1,20 @@
 
-public class Estudiante {
+public class Estudiante extends Persona{ 	//Extension de la clase Persona
+	
+	//atributos
+	
+		private String codigo;
+		private String email;
+		private String carrera;
+		
+		//Constructor 
+		// Aqui ya se aplica Herencia porque hereda atributos de la superclase Persona
+		public Estudiante(String nombre, int cedula, String codigo, String email, String carrera) {   //datos para crear el constructor estudiante
+			super(nombre, cedula);   //manda a ejecutar a super, de la superclase Persona, que se inicializa sus atributos
+			//inicializa los atributos propios de la clase estudiante
+			this.codigo = codigo;
+			this.email = email;
+			this.carrera = carrera;
+		}
 
 }
